58c291db46298e235bdfc82baa752c652d18319e
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-07-18 13:54:37 +0600

    shadcnUI setup and test using demo Login page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <div>
       <h3 className="text-center text-2xl underline font-medium">Home Page:  NextJS Press</h3>
 
+     <span className="">
+       <Button
+       size={"lg"}
+       >Click Me</Button>
+     </span>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next Press",
@@ -16,7 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
